aa6ded7166b752d5b95cd597a6365cb69a279cdd
ZongmingYin <holaus@sina.com>
2017-10-24 14:57:59 +0800

    2017-10-24 add RedBlackTree Code

--- OptimzingCollections/RedBlackTree.playground/Contents.swift
+++ OptimzingCollections/RedBlackTree.playground/Contents.swift
@@ -1,5 +1,24 @@
 //: Playground - noun: a place where people can play
 
+extension Sequence {
+    public func shuffled() -> [Iterator.Element] {
+        var contents = Array(self)
+        for i in 0..<contents.count {
+            //            #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+            //                let j = Int(arc4random_uniform(UInt32(count)))
+            //            #elseif os(Linux)
+            //                let j = random() % contents.count
+            //            #endif
+            let j = Int(arc4random_uniform(UInt32(contents.count))) // FIXME: 数组元素数量超过 2^32 时会挂
+            if i != j {
+                contents.swapAt(i, j)
+            }
+        }
+        return contents
+    }
+}
+
+
 // MARK: - 红黑树
 import UIKit
 /**
@@ -112,20 +131,214 @@ print(bigTree)
 /*
 extension RedBlackTree {
     public func inserting(_ element: Element) -> (tree: RedBlackTree, existingMember: Element?) {
-        let (tree, old) =
     }
 }
+ */
+
 
 extension RedBlackTree {
     @discardableResult
     public mutating func insert(_ element: Element) -> (inserted: Bool, memberAfterInsert: Element) {
-        let (tree, old) = insert(element)
+        let (tree, old) = inserting(element)
         self = tree
         return (old == nil, old ?? element)
         
     }
 }
-*/
+
+// 对于inserting,我们将参照由 Chris Okasaki 在 1999年首次发表的一个非常出色的模式匹配算法来进行实现:
+
+extension RedBlackTree {
+    public func inserting(_ element: Element) -> (tree: RedBlackTree, existingMember: Element?) {
+        let (tree, old) = _inserting(element)
+        switch tree {
+        case let .node(.red, value, left, right):
+            return (.node(.black, value, left, right), old)
+        default:
+            return (tree, old)
+        }
+    }
+}
+
+extension RedBlackTree {
+    func _inserting(_ element: Element) -> (tree: RedBlackTree, old: Element?) {
+        switch self {
+        case .empty:
+            return (.node(.red, element, .empty, .empty), nil)
+        case let .node(_, value, _, _) where value == element:
+            return (self, value)
+        case let .node(color, value, left, right) where value > element:
+            let (l, old) = left._inserting(element)
+            if let old = old { return (self, old) }
+            return (balanced(color, value, l, right), nil)
+        case let .node(color, value, left, right):
+            let (r, old) = right._inserting(element)
+            if let old = old { return (self, old) }
+            return (balanced(color, value, left, r), nil)
+        }
+    }
+}
+
+// 平衡
+// balanced方法的工作是检测现有的树是否违反了平衡的要求，如果是，则巧妙地重排节点，随即返回符合标准的树来进行修复。
+extension RedBlackTree {
+    func balanced(_ color: Color, _ value: Element, _ left: RedBlackTree, _ right: RedBlackTree) -> RedBlackTree {
+        switch (color, value, left, right) {
+        case let (.black, z, .node(.red, y, .node(.red, x, a, b), c), d):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, z, .node(.red, x, a, .node(.red, y, b, c)), d):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, x, a, .node(.red, z, .node(.red, y, b, c), d)):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, x, a, .node(.red, y, b, .node(.red, z, c, d))):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        default:
+            return .node(color, value, left, right)
+        }
+    }
+}
+
+var set = RedBlackTree<Int>.empty
+for i in (0 ... 20).shuffled(){
+    set.insert(i)
+}
+print(set)
+
+// 集合类型
+/**
+ 实现像 Collection 一样的协议恐怕是代数数据类型开始显得不那么方便的地方。我们需要定义 一个合适的索引类型，而最简单的方法就是直接使用一个元素本身作为索引，就像这样:
+ */
+extension RedBlackTree {
+    public struct Index {
+        fileprivate var value: Element?
+    }
+}
+extension RedBlackTree.Index: Comparable {
+    public static func ==(left: RedBlackTree<Element>.Index, right: RedBlackTree<Element>.Index) -> Bool {
+        return left.value == right.value
+    }
+    public static func <(left: RedBlackTree<Element>.Index, right: RedBlackTree<Element>.Index) -> Bool {
+        if let lv = left.value, let rv = right.value {
+            return lv < rv
+        }
+        return left.value != nil
+    }
+}
+
+extension RedBlackTree {
+    func min() -> Element? { // 查找最小元素:
+        switch self {
+        case .empty:
+            return nil
+        case let .node(_, value, left, _):
+            return left.min() ?? value
+        }
+    }
+    
+    func max() -> Element? { // 或者按照上面的查找最小元素的方法:
+        var node = self
+        var maximum: Element? = nil
+        while case let .node(_, value, _, right) = node {
+            maximum = value
+            node = right
+        }
+        return maximum
+    }
+}
+
+extension RedBlackTree: Collection {
+    
+    public var startIndex: Index { return Index(value: self.min()) }
+    public var endIndex: Index { return Index(value: self.max()) }
+    
+    public subscript(i: Index) -> Element {
+        return i.value!
+    }
+}
+
+extension RedBlackTree: BidirectionalCollection {
+    public func formIndex(after i: inout Index) {
+        let v = self.value(following: i.value!)
+        precondition(v.found)
+        i.value = v.next
+    }
+    
+    public func index(after i: Index) -> Index {
+        let v = self.value(following: value!)
+        precondition(v.found)
+        return Index(value: v.next)
+    }
+}
+
+// value(following:) 函数是一个 contains 的巧妙变体。虽然它的逻辑非常复杂，但是绝对值得你 再看一次:
+extension RedBlackTree {
+    func value(following element: Element) -> (found: Bool, next: Element?) {
+        switch self {
+        case .empty:
+            return (false, nil)
+        case .node(_, element, _, let right):
+            return (true, right.min())
+        case let .node(_, value, left, _) where value > element:
+            let v = left.value(following: element)
+            return (v.found, v.next ?? value)
+        case let .node(_, _, _, right):
+            return right.value(following: element)
+        }
+    }
+}
+
+extension RedBlackTree {
+    func value(preceding element: Element) -> (found: Bool, next: Element?) {
+        var node = self
+        var previous: Element? = nil
+        while case let .node(_, value, left, right) = node {
+            if value > element {
+                node = left
+            }else if value < element {
+                previous = value
+                node = right
+            }else {
+                return (true, left.max())
+            }
+        }
+        return (false, previous)
+    }
+}
+
+extension RedBlackTree {
+    public func formIndex(before i: inout Index) {
+        let v = self.value(preceding: i.value!)
+        precondition(v.found)
+        i.value = v.next
+    }
+    
+    public func index(before i: Index) -> Index {
+        let v = self.value(preceding: i.value!)
+        precondition(v.found)
+        return Index(value: v.next)
+    }
+}
+
+extension RedBlackTree {
+    public var count: Int { // 果我们忘记专⻔实现count，它的默认实现将会计算startIndex和endIndex之间的步数， 这样O(nlogn)比起我们的O(n)会慢得多。但是我们的实现也并没有什么值得宣扬的:它仍然需要访问树中每一个节点。
+        switch self {
+        case .empty:
+            return 0
+        case let .node(_, _, left, right):
+            return left.count + 1 + right.count
+        }
+    }
+}
+
+let newSet = set.lazy.filter {
+    $0 % 2 == 0
+    }.map {
+        "\($0)"
+}.joined(separator: ",")
+
+print(newSet)
+
+
 
 
 
